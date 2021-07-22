f3578f85faf30781af1ef93838aae7449357ac46
SajjdManafi <sajadmanafi8@gmail.com>
2021-07-22 21:05:26 +0430

    simple board

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,9 +1,12 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
@@ -15,13 +18,27 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception{
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main.fxml")));
+        Parent pane = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main.fxml")));
         stage.setTitle("Brick Breaker");
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(pane);
         stage.setScene(scene);
+        pane.getChildrenUnmodifiable().get(0).requestFocus();
         stage.show();
     }
 
+    @FXML
+    public void initialize(){
+        board.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                String keyName = keyEvent.getCode().getName();
+                switch (keyName) {
+                    case "Left" -> board.setX(board.getX() - 10);
+                    case "Right" -> board.setX(board.getX() + 10);
+                }
+            }
+        });
+    }
 
     public static void main(String[] args) {
         launch(args);

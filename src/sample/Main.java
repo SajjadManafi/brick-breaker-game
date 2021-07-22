e7469fcdbde2ac2732cdbd1f38132d04a19efe8d
SajjdManafi <sajadmanafi8@gmail.com>
2021-07-22 20:47:08 +0430

    add main.css file and add board rectangle

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,11 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.util.Objects;
 
 public class Main extends Application {
+
+    public Rectangle board;
 
     @Override
     public void start(Stage stage) throws Exception{
